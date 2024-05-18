914cf5aaa0dc6eeb05985359c6644421e86ac3cb
biluadev <biluadevcod@gmail.com>
2024-05-18 17:43:13 +0100

    feito o dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './global.css'
 import { router } from './routes'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop' />
-      <Toaster richColors/>
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
+        <Helmet titleTemplate='%s | pizza.shop' />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
